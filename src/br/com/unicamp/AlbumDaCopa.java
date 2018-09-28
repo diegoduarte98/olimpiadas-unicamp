1408c770cd9becc179106250c053d561f90caa8c
Diego Duarte <diegoduarte98@gmail.com>
2018-09-28 08:50:40 -0300

    Album da copa

---
+++ src/br/com/unicamp/AlbumDaCopa.java
@@ -0,0 +1,28 @@
+package br.com.unicamp;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class AlbumDaCopa {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Informe o total de figurinhas: ");
+        Integer totalDeFigurinhas = scanner.nextInt();
+
+        System.out.print("Informe o número de figurinhas compradas: ");
+        Integer numerosDeFigurinhasCompradas = scanner.nextInt();
+
+        Set<Integer> figurinhas = new HashSet<>();
+        Integer figurinhaComprada = 0;
+        while (figurinhaComprada < numerosDeFigurinhasCompradas) {
+            System.out.print("Informe o número da figurinha ("+ (figurinhaComprada+1) +") comprada: ");
+            figurinhas.add(scanner.nextInt());
+            figurinhaComprada++;
+        }
+
+        System.out.println("Figurinhas faltantes: "+ (totalDeFigurinhas - figurinhas.size()));
+    }
+}
